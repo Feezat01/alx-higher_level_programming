d97e5488c564b12612e3ce39d9fdee709bf9c196
Adeyemi Afeez <tbalogun27@gmail.com>
2022-11-30 07:50:06 -0800

    correct

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
-t1 in range(0, 10):
-        for digit2 in range(digit1 + 1, 10):
-                    if digit1 == 8 and digit2 == 9:
-                                    print("{}{}".format(digit1, digit2))
-                                            else:
-                                                            print("{}{}".format(digit1, digit2), end=", ")
+#!/usr/bin/python3
+for num1 in range(10):
+        for num2 in range(10):
+                    if num2 > num1 and num1 != 8:
+                                    print("{}{}".format(num1, num2), end=", ")
+                                            elif num1 == 8 and num2 == 9:
+                                                            print("{}{}".format(num1, num2))
+                                                                        break)
