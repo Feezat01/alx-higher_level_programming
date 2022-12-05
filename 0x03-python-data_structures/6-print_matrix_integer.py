82d600d611370f451249818fc767b0d2b83f46db
Adeyemi Afeez <tbalogun27@gmail.com>
2022-12-05 14:52:57 -0800

    Accept all file correctly

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+vi #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for y in range(len(matrix)):
+        for i in range(len(matrix[y])):
+            print("{:d}".format(matrix[y][i]), end="")
+            if i != (len(matrix[y]) - 1):
+                print(" ", end="")
+
+                print("")
